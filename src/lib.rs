b1f6ffa949796edc479851b986c1ef8b50ba505d
Xavier Lau <c.estlavie@icloud.com>
2019-02-12 23:47:06 +0800

    error detail

--- src/lib.rs
+++ src/lib.rs
@@ -146,10 +146,10 @@ impl<'a> Bypasser<'a> {
                                 resp.headers()[SET_COOKIE].to_owned()
                             );
                         }
-                        Err(e) => println!("At request_challenge(), text() {:?}", e)
+                        Err(e) => println!("At request_challenge() text(), {:?}", e)
                     }
                 }
-                Err(e) => println!("At, request_challenge(), send() {:?}", e)
+                Err(e) => println!("At, request_challenge() send(), {:?}", e)
             }
         }
     }
